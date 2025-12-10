998589916ea1e7118ff054fa51fcbe943aa1364c
aloosh977 <alisuleiman977@gmail.com>
2025-12-10 17:47:35 +0300

    animals

--- animals.py
+++ animals.py
@@ -1,2 +1,16 @@
 class Animal:
+    def sound(self):
+        pass
     
+class Dog(Animal):
+    def sound(self):
+        return "woof"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+
+dog=Dog()
+print(dog.sound())
+cat=Cat()
+print(cat.sound())
